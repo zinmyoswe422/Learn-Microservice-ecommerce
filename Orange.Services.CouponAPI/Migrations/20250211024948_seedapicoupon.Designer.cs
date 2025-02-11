04d0c84cd1eacbf8936d202a1bda7a129f07e1a6
zinmyoswe422 <zinmyoswe@cityparking.app>
2025-02-11 09:51:06 +0700

    Seed Coupon Database

--- Orange.Services.CouponAPI/Migrations/20250211024948_seedapicoupon.Designer.cs
+++ Orange.Services.CouponAPI/Migrations/20250211024948_seedapicoupon.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Orange.Services.CouponAPI.Data;
 
@@ -10,9 +11,11 @@ using Orange.Services.CouponAPI.Data;
 namespace Orange.Services.CouponAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250211024948_seedapicoupon")]
+    partial class seedapicoupon
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
