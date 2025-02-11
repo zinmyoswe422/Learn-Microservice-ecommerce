011a0362da40901bbc11cd451158bff564c0bf39
zinmyoswe422 <zinmyoswe@cityparking.app>
2025-02-11 14:30:58 +0700

    Base Service Implementation

--- Orange.Web/Service/IService/IBaseService.cs
+++ Orange.Web/Service/IService/IBaseService.cs
@@ -1,6 +1,9 @@
-﻿namespace Orange.Web.Service.IService
+﻿using Orange.Web.Models;
+
+namespace Orange.Web.Service.IService
 {
     public interface IBaseService
     {
+        Task<ResponseDto?> SendSync(RequestDto requestDto);
     }
 }
