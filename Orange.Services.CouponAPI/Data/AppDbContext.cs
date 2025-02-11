011543ba5015865843d2ec35be58520ad6c58dd6
zinmyoswe422 <zinmyoswe@cityparking.app>
2025-02-11 09:35:09 +0700

    Create AppDbContext

---
+++ Orange.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orange.Services.CouponAPI.Models;
+
+namespace Orange.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
