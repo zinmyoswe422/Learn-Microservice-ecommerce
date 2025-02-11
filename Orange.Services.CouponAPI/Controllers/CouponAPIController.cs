b74be7b927e3e8d5ba4a1669fff275d09ee62671
zinmyoswe422 <zinmyoswe@cityparking.app>
2025-02-11 16:46:28 +0700

    API Call in Action

--- Orange.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Orange.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -7,7 +7,8 @@ using Orange.Services.CouponAPI.Models.Dto;
 
 namespace Orange.Services.CouponAPI.Controllers
 {
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
+    [Route("api/coupon")]
     [ApiController]
     public class CouponAPIController : ControllerBase
     {
