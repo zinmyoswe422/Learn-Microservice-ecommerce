83feb8efa2ef895f4bf92e8cf763bfdc34566023
zinmyoswe422 <zinmyoswe@cityparking.app>
2025-02-11 09:23:28 +0700

    Create Coupon and CouponDTO in model

---
+++ Orange.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace Orange.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public string CouponName { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount {  get; set; }
+    }
+}
---
+++ Orange.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Orange.Services.CouponAPI.Models.Dto
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponName { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
