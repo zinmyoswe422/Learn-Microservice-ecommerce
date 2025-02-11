97073ea82ac796985593da982b399ac5c9ef541c
zinmyoswe422 <zinmyoswe@cityparking.app>
2025-02-11 13:40:02 +0700

    Create Request and Response DTO

---
+++ Orange.Web/Models/RequestDto.cs
@@ -0,0 +1,12 @@
+﻿using static Orange.Web.Utility.SD;
+
+namespace Orange.Web.Models
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
---
+++ Orange.Web/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Orange.Web.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
