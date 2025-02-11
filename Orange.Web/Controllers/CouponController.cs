f61ca399959e7fc36d4a32abb555062cb6237f44
zinmyoswe422 <zinmyoswe@cityparking.app>
2025-02-11 16:40:37 +0700

    Coupon Controller and CouponIndex function

---
+++ Orange.Web/Controllers/CouponController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Orange.Web.Models;
+using Orange.Web.Service.IService;
+
+namespace Orange.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+           _couponService = couponService;
+        }
+
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? list = new();
+
+            ResponseDto? response = await _couponService.GetAllCouponAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
+        }
+    }
+}
