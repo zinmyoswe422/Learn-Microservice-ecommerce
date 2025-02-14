49b011bd7504cd91bf4534b74c1d55574f99af3a
zinmyoswe422 <zinmyoswe@cityparking.app>
2025-02-14 15:01:25 +0700

    Part 3  - IAuth Service

---
+++ Orange.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Orange.Services.AuthAPI.Data;
+using Orange.Services.AuthAPI.Models;
+using Orange.Services.AuthAPI.Models.Dto;
+using Orange.Services.AuthAPI.Service.IService;
+
+namespace Orange.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> Register(RegisterationRequestDto registerationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Orange.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Orange.Services.AuthAPI.Models.Dto;
+
+namespace Orange.Services.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<UserDto> Register(RegisterationRequestDto registerationRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
